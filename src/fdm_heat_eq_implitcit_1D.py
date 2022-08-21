532b3383247c06c182273fe476fa3a6c20431258
Truong Dang <truong.dang@gft.com>
2022-08-21 13:40:57 +0700

    heat eq, implicit schem

--- src/fdm_heat_eq_implitcit_1D.py
+++ src/fdm_heat_eq_implitcit_1D.py
@@ -1,45 +1,8 @@
 import math
 import numpy
 
+from src.libs.tridiag import TriDiagonalMatrix
 from src.utils import visu1D
-
-
-def thomas(
-        a: numpy.ndarray,
-        b: numpy.ndarray,
-        c: numpy.ndarray,
-        r: numpy.ndarray,
-        u: numpy.ndarray,
-        n: int):
-    """
-
-    :param a:
-    :param b:
-    :param c:
-    :param r:
-    :param u:
-    :param n:
-    :return:
-    """
-    eps = 10**(-9)
-
-    if b[0] <= eps:
-        return
-
-    bet = b[0]
-    u[0] = r[0] / bet
-
-    gam = numpy.zeros(n)
-
-    for j in range(1, n):
-        gam[j] = c[j - 1] / bet
-        bet = b[j] - a[j] * gam[j]
-        if b[0] <= eps:
-            return
-        u[j] = (r[j] - a[j] * u[j - 1]) / bet
-
-    for j in range(n - 2, -1, -1):
-        u[j] = u[j] - gam[j + 1] * u[j + 1]
 
 
 if __name__ == "__main__":
@@ -70,7 +33,7 @@ if __name__ == "__main__":
 
     while time < Tend:
         a, b, c = A, B, C
-        thomas(a, b, c, wm, wn, M + 2)
+        TriDiagonalMatrix.thomas(a, b, c, wm, wn, M + 2)
         wn = wm
 
         time += Dt
